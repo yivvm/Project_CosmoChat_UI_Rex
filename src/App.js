707beb1402843a84751555a080fee7d375bf9ee0
Yiwen Zhang <yiwenmary@gmail.com>
2024-05-17 12:15:56 -0400

    create landing page

--- src/App.js
+++ src/App.js
@@ -1,120 +1,15 @@
 import { useState } from "react";
 
 import "./App.css";
-import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
-import {
-  MainContainer,
-  ChatContainer,
-  MessageList,
-  Message,
-  MessageInput,
-  TypingIndicator,
-} from "@chatscope/chat-ui-kit-react";
+import ChatPage from "./pages/Chat/ChatPage.js";
+import LandingPage from "./pages/Home/LandingPage.js";
 
 function App() {
-  const [typing, setTyping] = useState(false);
-
-  const [messages, setMessages] = useState([
-    {
-      message: "Hello, I am ChatGPT!",
-      sender: "ChatGPT",
-      direction: "incoming",
-    },
-  ]);
-
-  const handleSend = async (message) => {
-    const newMessage = {
-      message: message,
-      sender: "user",
-      direction: "outgoing",
-    };
-
-    const newMessages = [...messages, newMessage]; // all the old messages, + the new message
-
-    // update our messages state
-    setMessages(newMessages);
-
-    // set a type indicator (chatgpt is typing)
-    setTyping(true);
-
-    // process message to ChatGPT (send it over and see the response)
-    await processMessageToChatGPT(newMessages);
-  };
-
-  async function processMessageToChatGPT(chatMessages) {
-    // chatMessages { sender: "user" or "ChatGPT", message: "The message content here" }
-    // apiMessages { role: "user" or "assistant", content: "The message content here" }
-
-    let apiMessages = chatMessages.map((messageObject) => {
-      let role = "";
-      if (messageObject.sender === "ChatGPT") {
-        role = "assistant";
-      } else {
-        role = "user";
-      }
-      return {
-        role: role,
-        content: messageObject.message,
-      };
-    });
-
-    // role: "user" -> a message from the user, "assistant" -> a response from chatgpt
-    // "system" -> generally one initial message defining HOW we want chatgpt to talk
-
-    const systemMessage = {
-      role: "system",
-      content:
-        "Explain all concepts like a 10 years of experience software engineer.", // speak like a pirate. Explain like I am a 10 years of experience software engineer.
-    };
-
-    const apiRequestBody = {
-      model: "gpt-3.5-turbo",
-      messages: [systemMessage, ...apiMessages], // [message1, message2, message3]
-    };
-
-    await fetch("https://api.openai.com/v1/chat/completions", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${API_KEY}`,
-      },
-      body: JSON.stringify(apiRequestBody),
-    })
-      .then((data) => {
-        return data.json();
-      })
-      .then((data) => {
-        console.log(data);
-        console.log(data.choice[0].message.content);
-        setMessages([
-          ...chatMessages,
-          {
-            message: data.choice[0].message.content,
-            sender: "ChatGPT",
-            direction: "incoming",
-          },
-        ]);
-      });
-  }
-
   return (
     <div className="App">
       <div style={{ position: "relative", height: "600px", width: "300px" }}>
-        <MainContainer>
-          <ChatContainer>
-            <MessageList
-              scrollBehavior="smooth"
-              typingIndicator={
-                typing ? <TypingIndicator content="ChatGPT is typing" /> : null
-              }
-            >
-              {messages.map((message, index) => {
-                return <Message key={index} model={message} />;
-              })}
-            </MessageList>
-            <MessageInput placeholder="Type message here" onSend={handleSend} />
-          </ChatContainer>
-        </MainContainer>
+        <LandingPage />
+        {/* <ChatPage /> */}
       </div>
     </div>
   );
---
+++ src/pages/Home/LandingPage.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import ReactDom from "react-dom";
+
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator,
+} from "@chatscope/chat-ui-kit-react";
+
+export default function LandingPage() {
+  return (
+    <div
+      className="landing"
+      style={{
+        position: "relative",
+        height: "600px",
+        width: "300px",
+        border: "1px solid lightblue",
+      }}
+    >
+      <div className="image-container-landing">
+        <img
+          src="../images/landing.png"
+          style={{ height: "400px", width: "300px" }}
+        />
+      </div>
+      <h1>Welcome!</h1>
+      <h3>Receive Career Help from ReX!</h3>
+      <p>Start a conversation with ReX right now!</p>
+      <button>Start chat with ReX</button>
+    </div>
+  );
+}
